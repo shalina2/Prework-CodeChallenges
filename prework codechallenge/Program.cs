2881c02b085b42b744df11b265b0e865e2fd49a1
shalom <shalom.belaineh@gmail.com>
2019-01-11 12:22:57 -0800

    worked on challenge one

---
+++ prework codechallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace prework_codechallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("challenges");
+            int[] arr = new int[] { 2, 4, 4, 6, 8 };
+            Console.WriteLine("select number 2,4,4,6,8");
+            
+            int max = Int32.Parse(Console.ReadLine());
+            maxarr(arr, max);
+        }
+        static void maxarr(int[] arr,int max)
+        {
+            int result = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (max == arr[i])
+                    result += arr[i];
+            }
+            Console.WriteLine($"your score is {result}");
+        }
+    }
+}
