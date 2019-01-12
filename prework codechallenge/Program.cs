c20541ade0c3e383dd8850678569b29f7dedcb12
shalom <shalom.belaineh@gmail.com>
2019-01-11 18:11:48 -0800

    prework challenge1

--- prework codechallenge/Program.cs
+++ prework codechallenge/Program.cs
@@ -7,13 +7,13 @@ namespace prework_codechallenge
         static void Main(string[] args)
         {
             Console.WriteLine("challenges");
-            int[] arr = new int[] { 2, 4, 4, 6, 8 };
+            int[] arr = new int[]{ 2, 4, 4, 6, 8 };
             Console.WriteLine("select number 2,4,4,6,8");
-            
-            int max = Int32.Parse(Console.ReadLine());
+            Console.Write("");
+            int max=Int32.Parse(Console.ReadLine());
             maxarr(arr, max);
         }
-        static void maxarr(int[] arr,int max)
+       public static void maxarr(int[] arr,int max)
         {
             int result = 0;
             for(int i = 0; i < arr.Length; i++)
