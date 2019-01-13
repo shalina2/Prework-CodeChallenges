2eb342cd6775bc38b3767e58cb7434cd406f23b7
shalom <shalom.belaineh@gmail.com>
2019-01-12 18:58:23 -0800

    challenge3

--- prework codechallenge/Program.cs
+++ prework codechallenge/Program.cs
@@ -7,22 +7,26 @@ namespace prework_codechallenge
         static void Main(string[] args)
         {
             Console.WriteLine("challenges");
-            int[] arr = new int[]{ 2, 4, 4, 6, 8 };
+            int[] arr = new int[] { 2, 4, 4, 6, 8 };
             Console.WriteLine("select number 2,4,4,6,8");
             Console.Write("");
-            int max=Int32.Parse(Console.ReadLine());
+            int max = Int32.Parse(Console.ReadLine());
             maxarr(arr, max);
 
 
             Console.WriteLine("please choose your favorite year?");
             Console.Write("");
-            int year =Int32.Parse(Console.ReadLine());
+            int year = Int32.Parse(Console.ReadLine());
             leapyear(year);
+            Sequence();
+
+
+
         }
-       public static void maxarr(int[] arr,int max)
+        public static void maxarr(int[] arr, int max)
         {
             int result = 0;
-            for(int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
                 if (max == arr[i])
                     result += arr[i];
@@ -36,18 +40,50 @@ namespace prework_codechallenge
                 Console.WriteLine($"{year} is a leap year");
 
             }
-            else if(year%100==0)
+            else if (year % 100 == 0)
             {
                 Console.WriteLine($"{year} is not leap year");
             }
-            else if(year%400==0)
+            else if (year % 400 == 0)
             {
                 Console.WriteLine($"{year} is  leap year");
             }
-            else{
+            else
+            {
                 Console.WriteLine($"{year} is not a leap year");
             }
-            Console.ReadLine();
+
+        }
+
+        public static void Sequence()
+        {
+            Console.WriteLine("please enter how many numbers you want");
+            Console.Write("");
+            int seq = Int32.Parse(Console.ReadLine());
+            int[] array = new int[seq];
+            int sum = 0;
+            int product = 1;
+            for (int i = 0; i < seq; i++)
+            {
+                Console.WriteLine("please enter{0} element:", i + 1);
+                array[i] = Int32.Parse(Console.ReadLine());
+                sum += array[i];
+                product *= array[i];
+            }
+            if (sum < 0)
+            {
+                Console.WriteLine("not a perfect sequence");
+            }
+            else if (sum == product)
+            {
+                Console.WriteLine("perfect sequence");
+            }
+            else
+            {
+                Console.WriteLine("not a perfect match...");
+            }
+
         }
     }
 }
+    
